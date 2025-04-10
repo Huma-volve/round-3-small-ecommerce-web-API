b98351107f623b9fa10df011f27c3cbeaf865c91
Tarek Hesham <tarekghost6@gmail.com>
2025-04-10 22:12:19 +0200

    Make all models and relations

---
+++ app/Models/Products/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Products;
+
+use App\Models\Categories\Category;
+use App\Models\Categories\Subcategory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(ProductReview::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'product_category');
+    }
+
+    public function subcategories()
+    {
+        return $this->belongsToMany(Subcategory::class, 'product_subcategory');
+    }
+
+    public function details(): HasMany
+    {
+        return $this->hasMany(ProductDetail::class);
+    }
+}
