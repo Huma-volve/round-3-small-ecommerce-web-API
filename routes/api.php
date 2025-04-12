177c268e3d90ce9831967cb177af43eb9d5a9c2f
Tarek Hesham <tarekghost6@gmail.com>
2025-04-12 22:28:21 +0200

    [Testimonial] - Make model and register in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\TestimonialsController;
 use Illuminate\Support\Facades\Route;
 
 
 // Products API
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show'])->name('products.show');
+
+// Testimonials API
+Route::apiResource('testimonials', TestimonialsController::class);
